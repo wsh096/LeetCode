d7e05640e156ef4ed7192b54b742ad279c1b88f2
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-31 01:25:38 +0900

    Time: 1 ms (100%), Space: 60.5 MB (7.99%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //O(n);
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+        return maxSum;
+    }
+}
