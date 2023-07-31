d4169d7164248bf2b146c0eff75d99202009450c
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-07-31 16:36:42 +0900

    Time: 1 ms (99.33%), Space: 43.7 MB (79.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,16 +3,15 @@ import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(target - nums[0], 0);// 차를 이용해 값을 찾고(nums[i]에 해당하게 됨 target == 7,  -3 일시 4만 더 있으면 해결. 필요한 값이 키가 됨.), 인덱스를 value로 저장해 관리.
-        int [] arr = new int[2];
-        for(int i = 1; i < nums.length; i++){
-            if(map.containsKey(nums[i])){
-                arr[0] = map.get(nums[i]);
-                arr[1] = i;
-                break;
+        for(int i = 0; i < nums.length; i++){
+            int differ = target - nums[i];
+
+            if(map.containsKey(differ)){
+                return new int[] {map.get(differ), i};
+            }else{
+                map.put(nums[i], i);
             }
-            map.put(target-nums[i], i);
         }
-        return arr;
+        return new int[]{0,0};
     }
 }
