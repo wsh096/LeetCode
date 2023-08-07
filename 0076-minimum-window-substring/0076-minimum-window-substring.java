94d1a7766911b9917cb07327121014090bd71879
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-08-08 00:05:08 +0900

    Time: 3 ms (95.02%), Space: 43.4 MB (97.27%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(s.length() < t.length()) return "";
+
+        int[] tFrequency = new int[128];
+        int[] sFrequency = new int[128];
+
+        for(char c : t.toCharArray()){
+            tFrequency[c]++;
+        }
+
+        int left = 0;
+        int right = 0;
+        int minLength = Integer.MAX_VALUE;
+        int minStart = 0;
+        int requiredChars = t.length();
+
+        while(right < s.length()){
+            char rightChar = s.charAt(right);
+            if(tFrequency[rightChar] > 0) {
+                sFrequency[rightChar]++;
+                if(sFrequency[rightChar] <= tFrequency[rightChar]){
+                    requiredChars--;
+                }
+            }
+            while (requiredChars == 0 && left <= right) {
+                if(minLength > right - left){
+                    minLength = right - left;
+                    minStart = left;
+                }
+                char leftChar = s.charAt(left);
+                if(tFrequency[leftChar] > 0){
+                    sFrequency[leftChar]--;
+                    if(sFrequency[leftChar] < tFrequency[leftChar]){
+                        requiredChars++;
+                    }
+                }
+                left++;
+            }
+            right++;
+        }
+        return minLength == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLength + 1);
+    }
+}
