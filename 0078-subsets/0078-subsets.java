297532ff3904d5e9dd6cdd916ed31de036426b36
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-08-09 09:22:52 +0900

    Time: 1 ms (70.53%), Space: 41.9 MB (72.99%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    private void generateSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            generateSubsets(nums, i + 1, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
