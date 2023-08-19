17bfa3274c9457ddabc5339aaaba9bcbb4e153cb
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-08-20 00:38:35 +0900

    Time: 7 ms (86.98%), Space: 55.6 MB (23.11%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+    private void backtrack(String s, int start, List<String> currentList, List<List<String>> result){
+        if(start == s.length()){
+            result.add(new ArrayList<>(currentList));
+            return;
+        }
+            for(int end = start; end < s.length(); end++){
+                if(isPalindrome(s, start, end)){
+                    currentList.add(s.substring(start, end + 1));
+                    backtrack(s, end + 1, currentList, result);
+                    currentList.remove(currentList.size() - 1);
+                }
+            }
+        }
+    
+    private boolean isPalindrome(String s, int start, int end){
+        while(start < end) {
+            if(s.charAt(start++) != s.charAt(end--)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
