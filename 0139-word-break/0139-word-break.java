e49c04148e22b408713ec79311e0b6b2616528ce
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-08-22 00:04:38 +0900

    Time: 8 ms (37.45%), Space: 43.6 MB (29.99%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,19 +1,22 @@
+//import java.util.*;
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        int n = s.length();
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
-        int max_len = 0;
-        for(String word : wordDict){
-            max_len = Math.max(max_len, word.length());
-        }
-        for(int i = 1; i <= n; i++){
-            for(int j = i - 1; j >= Math.max(i - max_len - 1, 0); j--){
-                if(dp[j] && wordDict.contains(s.substring(j, i))){
-                    dp[i] = true; break;
-                }
+        Map<String, Boolean> memo = new HashMap<>();
+        Set<String> wordSet = new HashSet<>(wordDict);
+        return dfs(s, wordSet, memo);
+    }
+    
+    private boolean dfs(String s, Set<String> wordSet, Map<String, Boolean> memo) {
+        if (memo.containsKey(s)) return memo.get(s);
+        if (wordSet.contains(s)) return true;
+        for (int i = 1; i < s.length(); i++) {
+            String prefix = s.substring(0, i);
+            if (wordSet.contains(prefix) && dfs(s.substring(i), wordSet, memo)) {
+                memo.put(s, true);
+                return true;
             }
         }
-        return dp[n];
+        memo.put(s, false);
+        return false;
     }
 }
