a70de8e587025c75ea8216b241034f043e301023
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-08-28 00:05:28 +0900

    Time: 2 ms (100%), Space: 54.2 MB (90.53%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
+
+    ListNode sortList(ListNode p, ListNode q) {
+        if (p == null || p == q) {
+            return p;
+        }
+        boolean sorted = true;
+        ListNode curr = p;
+        while (curr.next != null && curr != q) {
+            if (curr.val > curr.next.val) {
+                sorted = false;
+                break;
+            }
+            curr = curr.next;
+        }
+        if (sorted) {
+            return p;
+        }
+        ListNode pre = p;
+        ListNode head = p;
+        curr = pre.next;
+        while (curr != null && curr != q) {
+            ListNode next = curr.next;
+            if (curr.val < p.val) {
+                pre.next = next;
+                curr.next = head;
+                head = curr;
+            } else {
+                pre = curr;
+            }
+            curr = next;
+        }
+        p.next = sortList(p.next, q);
+        return sortList(head, p);
+    }
+}
