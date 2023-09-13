80907fe96ef7ab4b5965891090788492d8fc8ae9
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-09-14 00:13:48 +0900

    Time: 19 ms (45.33%), Space: 43.5 MB (41.58%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int m = coins.length, n = amount;
+        int dp[][] = new int[m + 1][n + 1];
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                int t1 = Integer.MAX_VALUE;
+                if((i - 1) == 0){
+                    if(j % coins[i - 1] == 0){
+                        dp[i][j] = j / coins[i - 1];
+                    } else{
+                        dp[i][j] = (int)1e9;
+                    }
+                }
+                else{
+                    int t2 = dp[i - 1][j];
+                    if(coins[i - 1] <= j) {
+                        t1 = dp[i][j - coins[i - 1]] + 1;
+                    }
+                    dp[i][j] = Math.min(t1, t2);
+                }
+            }
+        }
+        if(dp[m][n] >= 1e9) return -1;
+        return dp[m][n];
+    }
+}
