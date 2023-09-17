baa2ed3ef407b0cef8e59ec13721e970dddf0257
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-09-17 15:53:44 +0900

    Time: 385 ms (19.33%), Space: 10 MB (89.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-    bool subsetSumTok(int n, int k, std::vector<int>& arr) {
-        std::vector<bool> prev(k + 1, false);
-
-        prev[0] = true;
-
-        if(arr[0] <= k) prev[arr[0]] = true;
-
-        for(int i = 1; i < n; i++){
-            std::vector<bool> cur(k + 1, false);
-            cur[0] = true;
-            for(int target = 1; target <= k; target++){
-                bool notTaken = prev[target];
-
-                bool taken = false;
-                if(arr[i] <= target) taken = prev[target - arr[i]];
-
-                cur[target] = notTaken || taken;                
-            }
-            prev = cur;
-        }
-        return prev[k];
-    }
-    bool canPartition(std::vector<int> & nums){
-        int n = nums.size();
-        if(n == 0) return false;
-
+    bool canPartition(vector<int>& nums) {
         int sum = 0;
         for(int num : nums) sum += num;
-
         if(sum % 2 != 0) return false;
-
-        int targetSum = sum / 2;
-        return subsetSumTok(n, targetSum, nums);
+        int req = sum / 2;
+        int n = nums.size();
+        vector<bool> dp(req + 1, false);
+        dp[0] = true;
+        for(int i = 0; i < n; i++){
+            for(int j = req; j >= nums[i]; j--){
+                dp[j] = dp[j] || dp[j - nums[i]];
+            }
+        }
+        return dp[req];
     }
 };
