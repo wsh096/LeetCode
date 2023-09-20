dd92df7f47d0da70c927fad4bdd6a892ac46322e
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-09-20 12:05:48 +0900

    Time: 5 ms (96.52%), Space: 41.1 MB (98.87%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,22 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        //slicing
-        for(int i = 0; i <= m - n; i++){
-            String target = s2.substring(i, i + n);
-            int [] goal = new int[26];
-            char[] go = s1.toCharArray();
-            char[] find = target.toCharArray();
-            for(int j = 0; j < n; j++){
-                goal[go[j] - 'a']++;
-                goal[find[j] - 'a']--;
-            }
-            for(int j = 0; j < 26; j++){
-                if(goal[j] != 0) break;
-                if(j == 25 && goal[j] == 0) return true;
-            }
+        
+        if(s1.length()>s2.length())return false;
+        int[] data = new int[26];
+        int[] test = new int[26];
+        
+        for(char c : s1.toCharArray())data[c-'a']++;
+        
+        for(int i=0;i<s1.length();i++)test[s2.charAt(i)-'a']++;        
+        int n=s1.length(); 
+
+        for(int i=0;i<s2.length()-n;i++){
+            if(equalsString(test, data))return true;
+            test[s2.charAt(i+n)-'a']++;     
+            test[s2.charAt(i)-'a']--;            
         }
-        return false;
+        return equalsString(test, data);
+    }
+    private boolean equalsString(int[] s1, int[] s2){
+        
+        for(int i=0;i<26;i++)
+            if(s1[i]!=s2[i])return false;        
+        return true;
     }
 }
