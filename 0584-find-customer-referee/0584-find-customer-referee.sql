063956dc96a7016c4d054d365bd0a61ffaa4583c
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-09-23 22:51:07 +0900

    Time: 795 ms (98.4%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select name 
-from customer 
-where referee_id is null or referee_id!=2;
+SELECT name
+FROM Customer
+WHERE referee_id IS NULL OR referee_id != 2
