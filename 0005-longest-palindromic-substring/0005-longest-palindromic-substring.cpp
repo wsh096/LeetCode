ab48921bc6ad62f438f72401f6ceeaf50fca0fe9
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-09-29 17:55:18 +0900

    Time: 875 ms (13.94%), Space: 7.3 MB (69.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,29 @@
 class Solution {
-private:
-    bool check(string &s, int left, int right){
-        while(left < right){
-            if(s[left] != s[right]){
-                return false;
+    private:
+        bool check(string &s, int left, int right){
+            while(left < right){
+                if(s[left] != s[right]){
+                    return false;
+                }
+                left++; right--;
             }
-            left++; right--;
+            return true;
         }
-        return true;
-    }
-public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        int start = 0;
-        int max_len = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(check(s, i, j)){
-                    if(j - i + 1 > max_len){
-                        max_len = j - i + 1;
-                        start = i;
+    public:
+        string longestPalindrome(string s){
+            int n = s.size();
+            int start = 0;
+            int max = 0;
+            for(int i = 0; i < n; i++){
+                for(int j = i; j < n; j++){
+                    if(check(s, i, j)){
+                        if(j - i + 1 > max){
+                            max = j - i + 1;
+                            start = i;
+                        }
                     }
                 }
             }
+            return s.substr(start, max);
         }
-        return s.substr(start, max_len);
-    }
-};
+    };      
