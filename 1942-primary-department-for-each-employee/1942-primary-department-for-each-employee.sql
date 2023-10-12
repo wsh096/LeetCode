5d02a0e6c61193ffd190ad4b717bf1bd9e12dfc6
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-10-13 07:08:26 +0900

    Time: 964 ms (89.85%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT employee_id, department_id
+SELECT employee_id, MAX(department_id) AS department_id
 FROM Employee
 GROUP BY employee_id
 HAVING COUNT(department_id) = 1
