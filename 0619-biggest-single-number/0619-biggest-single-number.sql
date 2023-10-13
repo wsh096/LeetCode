affe4375b01482a4dbd562e71f42d4ad862c5d5d
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-10-13 22:07:56 +0900

    Time: 727 ms (85.52%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,7 +1,6 @@
-SELECT 
-    (SELECT num
-     FROM MyNumbers
-     GROUP BY num
-     HAVING COUNT(num) = 1
-     ORDER BY num DESC
-     LIMIT 1) AS num;
+# Write your MySQL query statement below
+SELECT max(num) num
+FROM (SELECT num
+      FROM MyNumbers
+      GROUP BY num
+      HAVING COUNT(num) = 1) num
