f2ef351046f595a4a3e67aaaba503d6e1127ae7c
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-07 18:54:45 +0900

    Time: 24 ms (5.05%), Space: 15.3 MB (5.05%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    int answer = 100001;
-    void deleteString(string& s){
-        if((s.find("AB") == string::npos)
-        && (s.find("CD") == string::npos)){
-            answer = min((int)s.size(), answer);
-            return;
-        }
-        if(s.find("AB") != string::npos){
-            int found = s.find("AB");   
-            s = s.substr(0, found) + s.substr(found + 2);
-            deleteString(s);
-        }else if(s.find("CD") != string::npos){
-            int found = s.find("CD");   
-            s = s.substr(0, found) + s.substr(found + 2);
-            deleteString(s);
-        }
-    }
     int minLength(string s) {
-        deleteString(s);
-        return answer;
+        while(s.find("AB") != string::npos || s.find("CD") != string::npos){
+            if(s.find("AB") != string::npos){
+                int found = s.find("AB");
+                s = s.substr(0, found) + s.substr(found + 2);
+            }
+            if(s.find("CD") != string::npos){
+                int found = s.find("CD");
+                s = s.substr(0, found) + s.substr(found + 2);
+            }
+        }
+        return (int)s.size();
     }
 };
