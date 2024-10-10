fbad8825c03493a4e581df8994de780a951b5408
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-10 11:01:57 +0900

    Time: 6 ms (25.89%), Space: 17.6 MB (25.92%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -12,16 +12,11 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        return check(root -> left, root -> right);
+        return check(root->left, root->right);
     }
-private:
     bool check(TreeNode* n1, TreeNode* n2){
-        if(!n1 && !n2){
-            return true;
-        }
-        if(!n1 || !n2){
-            return false;
-        }
-        return n1 -> val == n2 ->val && check(n1 -> left, n2 -> right) && check(n1 -> right, n2 -> left);
-        }
+        if(!n1 && !n2) return true;
+        else if(!n1 || !n2) return false;
+        return n1 -> val == n2 -> val && check(n1 -> left, n2 -> right) && check(n1 -> right, n2 -> left);
+    }
 };
