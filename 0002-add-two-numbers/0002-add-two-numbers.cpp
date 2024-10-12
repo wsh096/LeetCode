dd027586d3adbb1ca4d203ee6f2c84dacc253d78
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-12 23:10:01 +0900

    Time: 24 ms (48.53%), Space: 77.6 MB (42.88%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,15 +14,15 @@ public:
         ListNode* answer = new ListNode();
         ListNode* dumy = answer;
         int carry = 0;
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int x = l1 != nullptr ? l1->val : 0;
-            int y = l2 != nullptr ? l2->val : 0;
+        while (l1 || l2 || carry != 0) {
+            int x = l1 ? l1->val : 0;
+            int y = l2 ? l2->val : 0;
             int sum = x + y + carry;
             carry = sum / 10;
             dumy->next = new ListNode(sum % 10);
             dumy = dumy->next;
-            l1 = l1 != nullptr ? l1->next : nullptr;
-            l2 = l2 != nullptr ? l2->next : nullptr;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
         }
         return answer->next;
     }
