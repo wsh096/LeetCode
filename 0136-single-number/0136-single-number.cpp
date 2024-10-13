0f73b725c6582d6cd0477443e290eeddda7dd894
Wang_Seok_Hyeon <wsh096@gmail.com>
2024-10-13 22:40:52 +0900

    Time: 18 ms (34.42%), Space: 19.6 MB (26.93%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> find_unique_num;
-        for(int n : nums)
-                find_unique_num[n]++;
-        for(auto n : find_unique_num){
-            if(n.second == 1) return n.first;
+        int answer = 0;
+        for(int i : nums){
+            answer ^= i;
         }
-        return 0;
+        return answer;
     }
 };
